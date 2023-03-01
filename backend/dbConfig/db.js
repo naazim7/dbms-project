e53cb4fc8cdad09acaa2387893dde1bff3824887
Mobarak Hossain Nazim <59817491+naazim7@users.noreply.github.com>
2023-03-01 11:36:11 +0600

    Delete backend directory

--- backend/dbConfig/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'qna'
-});
-
-module.exports = connection;
